dbd4c7caeeb3a1d374a38a2cca33b36e99375050
Nathan LeBlanc <nathanjleblanc@gmail.com>
2017-11-29 12:20:16 -0500

    Environments and Surface pages

--- js/app.js
+++ js/app.js
@@ -2,5 +2,10 @@ $(document).foundation();
 
 
 $('#gallery-selector').lightGallery({
-  selector: '.item'
+  selector: '.item',
+  cssEasing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+  fullscreen: false,
+  download: false,
+  autoplay: false,
+  share: false
 });
