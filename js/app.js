d2da761ebda1eedea362152cb99c5a6d5be5f3d9
Nathan LeBlanc <nathanjleblanc@gmail.com>
2017-11-29 21:55:24 -0500

    Particles and Animation

--- js/app.js
+++ js/app.js
@@ -1,17 +1,16 @@
 $(document).foundation();
 
 
-
 //Homepage animations
 
 //Title name animation
 $(document).ready(function() {
   
-  $('#alexander').addClass('animated fadeIn').delay(500);
-  
-  $('#alexander').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-    $('#winkler').addClass('animated fadeIn');
+  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+  particlesJS.load('particles-js', 'js/particlesjs-config.json', function() {
+    console.log('callback - particles.js config loaded');
   });
   
   
 });
+
